111b203e074741c9fda446ffaed16016581e7628
f.yousefi <f.yousefi2@gmail.com>
2023-02-26 14:20:30 +0330

    Build a new pipe to truncate string of char

--- angular-test-and-design-for-testability/src/app/app.module.ts
+++ angular-test-and-design-for-testability/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TruncatePipe } from './pipes/truncate.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TruncatePipe
   ],
   imports: [
     BrowserModule
