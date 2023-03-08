8e6a9ddfdee6a3f86dcf7735009935e60e2c7c67
f.yousefi <f.yousefi2@gmail.com>
2023-03-08 19:35:19 +0330

    Declare the router module

--- angular-rest-application/src/app/app.module.ts
+++ angular-rest-application/src/app/app.module.ts
@@ -8,7 +8,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
