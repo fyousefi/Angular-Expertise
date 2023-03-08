a25a69d3228f30e53fe38f757e60be6dbfdf6d91
f.yousefi <f.yousefi2@gmail.com>
2023-03-08 20:35:19 +0330

    Import and declare the app router module

--- angular-rest-application/src/app/app.module.ts
+++ angular-rest-application/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app.routes';
 
 import { AppComponent } from './app.component';
 
