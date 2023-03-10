6a1a14399ba8ab2d520ce4e11741283115d2cb6e
f.yousefi <f.yousefi2@gmail.com>
2023-03-10 20:35:19 +0330

    Create about component

--- angular-rest-application/src/app/app.module.ts
+++ angular-rest-application/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app.routes';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
