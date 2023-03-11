77c3655a83cefc2ead5254e015598584fa737e80
f.yousefi <f.yousefi2@gmail.com>
2023-03-11 21:43:19 +0330

    Fixed the router imports

--- angular-rest-application/src/app/app.routes.ts
+++ angular-rest-application/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
-import { ContactComponent } from './compponents/contact/contact.component';
+import { ContactComponent } from './components/contact/contact.component';
 import { HomeComponent } from './components/home/home.component';
 
 const ROUTES: Routes = [
