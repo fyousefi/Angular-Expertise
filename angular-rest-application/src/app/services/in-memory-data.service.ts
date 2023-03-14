a4d7b6fa64b8d3b84a90c25b0a6554d6d7b767ff
f.yousefi <f.yousefi2@gmail.com>
2023-03-14 09:41:29 +0330

    Add create db

--- angular-rest-application/src/app/services/in-memory-data.service.ts
+++ angular-rest-application/src/app/services/in-memory-data.service.ts
@@ -5,7 +5,23 @@ import { User } from '../user';
 @Injectable({
   providedIn: 'root'
 })
-export class InMemoryDataService {
+export class InMemoryDataService implements InMemoryDbService{
 
   constructor() { }
+
+  createDb() {
+    const users = [
+        { id: 11, first_name: 'Matthew', last_name: 'Collins', avatar: 'assets/img/user.jpg' },
+        { id: 12, first_name: 'Kyle', last_name: 'Munoz', avatar: 'assets/img/user.jpg' },
+        { id: 13, first_name: 'Laura', last_name: 'Adkins', avatar: 'assets/img/user.jpg' },
+        { id: 14, first_name: 'Kara', last_name: 'Vargas', avatar: 'assets/img/user.jpg' },
+        { id: 15, first_name: 'Austin', last_name: 'Sawyer', avatar: 'assets/img/user.jpg' },
+        { id: 16, first_name: 'Bruce', last_name: 'Bautista', avatar: 'assets/img/user.jpg' },
+        { id: 17, first_name: 'Lisa', last_name: 'Griffin', avatar: 'assets/img/user.jpg' },
+        { id: 18, first_name: 'Jason', last_name: 'Burns', avatar: 'assets/img/user.jpg' },
+        { id: 19, first_name: 'Tyler', last_name: 'Romero', avatar: 'assets/img/user.jpg' },
+        { id: 20, first_name: 'Kathryn', last_name: 'Williams', avatar: 'assets/img/user.jpg' }
+    ];
+    return { users };
+  }
 }
