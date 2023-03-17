95096c10108f1ce90e4ebcda1236d23ad71c1031
f.yousefi <f.yousefi2@gmail.com>
2023-03-17 10:30:29 +0330

    Add http client in memory web api module

--- angular-rest-application/src/app/app.module.ts
+++ angular-rest-application/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app.routes';
 import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
