4bd8d73ec6ab4e842bc7cb7f21f5aede3c53faf5
f.yousefi <f.yousefi2@gmail.com>
2023-03-19 09:46:29 +0330

    Fixed ternery if indention

--- angular-rest-application/src/app/services/in-memory-data.service.ts
+++ angular-rest-application/src/app/services/in-memory-data.service.ts
@@ -25,7 +25,6 @@ export class InMemoryDataService implements InMemoryDbService{
     return { users };
   }
   genId(users: User[]): number {
-    return users.length > 0 ? Math.max(...users.map(user => user.id))
-    + 1 : 11;
+    return users.length > 0 ? Math.max(...users.map(user => user.id)) + 1 : 11;
   }
 }
