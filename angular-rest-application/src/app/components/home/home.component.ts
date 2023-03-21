7f2f8c9cdf50e6d385883a553dc1a8bbcb937607
f.yousefi <f.yousefi2@gmail.com>
2023-03-22 01:23:29 +0330

    Import user model

--- angular-rest-application/src/app/components/home/home.component.ts
+++ angular-rest-application/src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ReqresService } from 'src/app/services/reqres.service';
+import { User } from 'src/app/user';
 
 @Component({
   selector: 'app-home',
@@ -9,5 +10,7 @@ import { ReqresService } from 'src/app/services/reqres.service';
 export class HomeComponent {
 
     constructor(private reqresService: ReqresService) {}
+
+
 
 }
