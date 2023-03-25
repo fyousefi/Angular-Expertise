8cf324b05af449a0ce9a34e57522da5c281c1a70
f.yousefi <f.yousefi2@gmail.com>
2023-03-25 05:57:29 +0330

    Import user model

--- angular-rest-application/src/app/services/reqres.service.ts
+++ angular-rest-application/src/app/services/reqres.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { User } from '../user';
 
 @Injectable({
   providedIn: 'root'
@@ -10,4 +11,6 @@ export class ReqresService {
   private url = 'api/users';
 
   constructor(private http: HttpClient) { }
+
+
 }
