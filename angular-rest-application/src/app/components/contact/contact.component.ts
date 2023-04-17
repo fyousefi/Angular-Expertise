65617048c97207620bdbccf76ae2a2a85f2552d0
fyousefi <f.yousefi2@gmail.com>
2023-04-17 07:57:47 -0700

    Implements the oninit

--- angular-rest-application/src/app/components/contact/contact.component.ts
+++ angular-rest-application/src/app/components/contact/contact.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+
+  constructor(){}
+
+  ngOnInit() {
+      
+  }
 
 }
