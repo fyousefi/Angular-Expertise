27516e38bc536a879dc510e037984ac918bf3fc8
fyousefi <f.yousefi2@gmail.com>
2023-04-20 10:00:50 -0700

    Add submitted property

--- angular-rest-application/src/app/components/contact/contact.component.ts
+++ angular-rest-application/src/app/components/contact/contact.component.ts
@@ -9,7 +9,8 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class ContactComponent implements OnInit {
 
   messageForm: FormGroup;
-  
+  submitted = false;
+
 
   constructor(){}
 
