bbd153cb6a937a3ed1ce7c9a0d83c5b210033e04
fyousefi <f.yousefi2@gmail.com>
2023-04-22 12:03:53 -0700

    Inject a variable of type FormBuilder

--- angular-rest-application/src/app/components/contact/contact.component.ts
+++ angular-rest-application/src/app/components/contact/contact.component.ts
@@ -12,7 +12,12 @@ export class ContactComponent implements OnInit {
   submitted = false;
   success = false;
 
-  constructor(private formBuilder: FormBuilder){}
+  constructor(private formBuilder: FormBuilder){
+    this.messageForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      message: ['', Validators.required]
+    });
+  }
 
   ngOnInit() {
       
