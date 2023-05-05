7ca3cc9279ce1f42db6ce001c5e9118614b0e3f3
F.Yousefi <f.yousefi2@gmail.com>
2023-05-05 13:12:00 +0330

    Import router

--- angular-rest-application/src/app/components/user/user-add.component.ts
+++ angular-rest-application/src/app/components/user/user-add.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-add',
