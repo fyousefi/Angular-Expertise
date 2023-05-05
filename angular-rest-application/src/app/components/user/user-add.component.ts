a1c0a98a849aac973d6b92f222d8902c5b76bd16
F.Yousefi <f.yousefi2@gmail.com>
2023-05-05 13:14:00 +0330

    Import user interface

--- angular-rest-application/src/app/components/user/user-add.component.ts
+++ angular-rest-application/src/app/components/user/user-add.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ReqresService } from 'src/app/services/reqres.service';
+import { User } from 'src/app/user';
 
 @Component({
   selector: 'app-user-add',
