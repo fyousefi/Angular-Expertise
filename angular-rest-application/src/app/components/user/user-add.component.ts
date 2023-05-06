040f4c88e7ff518c6a59245db0004cfd3ac5c773
F.Yousefi <f.yousefi2@gmail.com>
2023-05-06 13:13:00 +0330

    Import ReqresService

--- angular-rest-application/src/app/components/user/user-add.component.ts
+++ angular-rest-application/src/app/components/user/user-add.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ReqresService } from 'src/app/services/reqres.service';
 
 @Component({
   selector: 'app-user-add',
