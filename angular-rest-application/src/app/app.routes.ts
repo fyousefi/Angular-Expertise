5493efded5e4ddad57556f0c2d3a71bc9a987011
f.yousefi <f.yousefi2@gmail.com>
2023-11-05 14:36:19 +0330

    Add angular router module

--- angular-rest-application/src/app/app.routes.ts
+++ angular-rest-application/src/app/app.routes.ts
@@ -1,1 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'path', component: FeatureComponent },
+    { path: '**', component: PageNotFoundComponent },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
 
