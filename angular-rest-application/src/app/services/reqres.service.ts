7cedf3043dfe5a801cb6bac4386ee7c1837d3b8a
F.Yousefi <f.yousefi2@gmail.com>
2023-12-05 00:19:14 -0800

    Import RxJs operators

--- angular-rest-application/src/app/services/reqres.service.ts
+++ angular-rest-application/src/app/services/reqres.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../user';
+import { catchError, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
