318b172c470868acbb759c0504c64de6f01d5d3e
F.Yousefi <f.yousefi2@gmail.com>
2023-12-10 07:16:00 -0800

    Implements oninit

--- angular-rest-application/src/app/components/user/user-detail.component.ts
+++ angular-rest-application/src/app/components/user/user-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ReqresService } from 'src/app/services/reqres.service';
 import { User } from 'src/app/user';
@@ -8,7 +8,7 @@ import { User } from 'src/app/user';
   templateUrl: './user-detail.component.html',
   styleUrls: ['./user-detail.component.css']
 })
-export class UserDetailComponent {
+export class UserDetailComponent implements OnInit {
 
     user: User = {
         id: 0,
@@ -25,5 +25,7 @@ export class UserDetailComponent {
         reqresService.getUser(params['id']).subscribe((res: User) => this.user = res);
       });
     }
+
+    ngOnInit() {}
 
 }
