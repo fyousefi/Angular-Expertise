9f4d475c7a311707488135d16c9c5a3703bc5617
F.Yousefi <f.yousefi2@gmail.com>
2023-12-11 02:33:42 -0800

    Fixed the issue

--- angular-rest-application/src/app/services/reqres.service.ts
+++ angular-rest-application/src/app/services/reqres.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 import { User } from '../user';
+
 import { catchError, map } from 'rxjs/operators';
 
 @Injectable({
@@ -12,8 +13,8 @@ export class ReqresService {
   private url = 'api/users';
 
   httpOptions = {
-    headers: new HttpHeaders({  })
-  }
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
   constructor(private http: HttpClient) { }
 
